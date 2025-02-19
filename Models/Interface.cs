a5ae9a99bd95ea4f06426a711cba571eb9a0052e
Muffinger <marvin-boesl@gmx.net>
2025-02-19 21:57:44 +0100

    Projektdateien hinzufügen.

---
+++ Models/Interface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+
+namespace ArenaHub_Avalonia.Models
+{
+    public class Interface
+    {
+        private SerialPort serialPort;
+
+        public void OpenPort(string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                ClosePort();
+            }
+
+            serialPort = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+            serialPort.Open();
+            Console.WriteLine($"Port {portName} geöffnet.");
+        }
+
+        public void ClosePort()
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                Console.WriteLine("Port geschlossen.");
+            }
+        }
+
+        public List<string> GetAvailablePorts()
+        {
+            return new List<string>(SerialPort.GetPortNames());
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+
+namespace ArenaHub_Avalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> AvailablePorts { get; set; }
+        public string SelectedPort { get; set; }
+        public ObservableCollection<int> BaudRates { get; set; }
+        public int SelectedBaudRate { get; set; }
+        public ObservableCollection<int> DataBitsOptions { get; set; }
+        public int SelectedDataBits { get; set; }
+        public ObservableCollection<Parity> Parities { get; set; }
+        public Parity SelectedParity { get; set; }
+        public string LogMessages { get; set; }
+
+        public ObservableCollection<PortViewModel> Ports { get; set; }
+
+        public MainWindowViewModel()
+        {
+            AvailablePorts = new ObservableCollection<string>(SerialPort.GetPortNames());
+            BaudRates = new ObservableCollection<int> { 9600, 115200 };
+            DataBitsOptions = new ObservableCollection<int> { 7, 8 };
+            Parities = new ObservableCollection<Parity> { Parity.None, Parity.Odd, Parity.Even };
+
+            Ports = new ObservableCollection<PortViewModel>
+            {
+                new PortViewModel { PortName = "Input 1" },
+                new PortViewModel { PortName = "Input 2" },
+                new PortViewModel { PortName = "Output Display" }
+            };
+        }
+    }
+
+    public class PortViewModel : ReactiveObject
+    {
+        public string PortName { get; set; }
+        public string SelectedPort { get; set; }
+        public string ConnectionStatus => string.IsNullOrEmpty(SelectedPort) ? "Disconnected" : "Connected";
+    }
+}
